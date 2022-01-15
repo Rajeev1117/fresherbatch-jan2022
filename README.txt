ff9608192dcd59e12a5e9cdf833db0dfef660021
Rajeev Kumar <me@mydomain.com>
2022-01-15 20:37:32 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+this is section1 assignment
+HTML
+CSS
+<<<<<<< HEAD
+style.css
+=======
+Rajeev
+javascript
+>>>>>>> js-assignments
---
+++ rajeev.js
@@ -0,0 +1,9 @@
+// store input numbers
+const num1 = parseInt(prompt('Enter the Rfirst number '));
+const num2 = parseInt(prompt('Enter the Rsecond number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
